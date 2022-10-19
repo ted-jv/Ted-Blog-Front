5b01962759cc2a1dc2f96104e014d3bb81d9a279
ted-jv <wkejrdldls@gmail.com>
2022-10-19 23:58:19 +0900

    Feat: express 기본 형식

--- back/app.js
+++ back/app.js
@@ -1,8 +1,35 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.end('Hello');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello express');
 });
-server.listen(3065, () => {
+
+app.get('/api', (req, res) => {
+  res.send('hello');
+});
+
+app.get('/api/posts', (req, res) => {
+  res.json([
+    { id: 1, content: 'hello' },
+    { id: 2, content: 'hello2' },
+    { id: 3, content: 'hello3' },
+  ]);
+});
+
+app.post('/api/post', (req, res) => {
+  res.json('작성 완료');
+});
+
+app.post('/api', (req, res) => {
+  res.json('작성 완료');
+});
+
+app.delete('/api', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(3065, () => {
   console.log('서버 실행 중');
 });
