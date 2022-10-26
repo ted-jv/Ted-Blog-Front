652e65dbe86bcc2624cf58d10ace98b7e97926bd
ted-jv <wkejrdldls@gmail.com>
2022-10-26 23:57:01 +0900

    Feat: 추가 - configModule
    app.module import에 configModule 추가
    &  .env 파일 추가
    &  nest/config 설치

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [], //여기에 socket i.o 모듈, 인증 모듈 등 만들어서 넣으면 된다.
+  imports: [ConfigModule.forRoot()], //여기에 socket i.o 모듈, 인증 모듈 등 만들어서 넣으면 된다.
   controllers: [AppController], // express의 router 역할
   providers: [AppService],
 })
